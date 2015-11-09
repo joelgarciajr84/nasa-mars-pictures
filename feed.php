2b65c6ffecd0021308be825301ca1bd828eee525
Joel Garcia Jr <joel.garciajr84@gmail.com>
2015-11-09 00:08:03 -0200

    add feed

--- feed.php
+++ feed.php
@@ -1,5 +1,7 @@
 <?php
-header('Content-Type: text/xml; charset=utf-8', true);
+//header('Content-Type: application/xml');
+header('Content-Type: application/rss+xml; charset=utf-8');
+echo '<?xml version="1.0" encoding="utf-8"?>';
 
 require 'rover.php';
 $Mars = new HelloMars();

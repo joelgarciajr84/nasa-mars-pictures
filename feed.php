4ac77397909533f85e3ac2dd961a4eb1ebd8f475
Joel Garcia Jr <joel.garciajr84@gmail.com>
2015-11-09 09:00:09 -0200

    fix feed

--- feed.php
+++ feed.php
@@ -2,8 +2,7 @@
 header("Content-type: text/xml");
 
 echo "<?xml version='1.0' encoding='UTF-8'?>
-<rss version='2.0'
-xmlns:content='http://purl.org/rss/1.0/modules/content/'
+<rss version='2.0' xmlns:w='shttp://tempuri.org'>
 	xmlns:wfw='http://wellformedweb.org/CommentAPI/'
 	xmlns:dc='http://purl.org/dc/elements/1.1/'
 	xmlns:atom='http://www.w3.org/2005/Atom'
@@ -38,12 +37,12 @@ foreach ($images as $image) {
         'rover_name'    =>  $image[$i]->rover->name
     );*/
     $rssfeed .= '<item>';
-    $rssfeed .= '<dc:id>' . $image[$i]->id. '</dc:id>';
-    $rssfeed .= '<dc:sol>' . $image[$i]->sol. '</dc:sol>';
-    $rssfeed .= '<dc:camera_name>' . $image[$i]->camera->name . '</dc:camera_name>';
-    $rssfeed .= '<dc:img_src>' . $image[$i]->img_src . '</dc:img_src>';
-    $rssfeed .= '<dc:earth_date>' . date("d/m/Y",strtotime($image[$i]->earth_date)) . '</dc:earth_date>';
-    $rssfeed .= '<dc:rover_name>' . $image[$i]->rover->name. '</dc:rover_name>';
+    $rssfeed .= '<w:id>' . $image[$i]->id. '</w:id>';
+    $rssfeed .= '<w:sol>' . $image[$i]->sol. '</w:sol>';
+    $rssfeed .= '<w:camera_name>' . $image[$i]->camera->name . '</w:camera_name>';
+    $rssfeed .= '<w:img_src>' . $image[$i]->img_src . '</w:img_src>';
+    $rssfeed .= '<w:earth_date>' . date("d/m/Y",strtotime($image[$i]->earth_date)) . '</w:earth_date>';
+    $rssfeed .= '<w:rover_name>' . $image[$i]->rover->name. '</w:rover_name>';
     $rssfeed .= '<pubDate>' . date("D, d M Y H:i:s O", strtotime($image[$i]->earth_date)) . '</pubDate>';
     $rssfeed .= '</item>';
     }

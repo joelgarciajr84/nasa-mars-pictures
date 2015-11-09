e7443bba4b5a2b842a608b3ec9797c090213d9e9
Joel Garcia Jr <joel.garciajr84@gmail.com>
2015-11-09 01:04:48 -0200

    add feed

--- feed.php
+++ feed.php
@@ -37,6 +37,7 @@ foreach ($images as $image) {
     $rssfeed .= '<id>' . $image[$i]->id. '</id>';
     $rssfeed .= '<sol>' . $image[$i]->sol. '</sol>';
     $rssfeed .= '<camera_name>' . $image[$i]->camera->name . '</camera_name>';
+    $rssfeed .= '<img_src>' . $image[$i]->img_src . '</img_src>';
     $rssfeed .= '<earth_date>' . date("d/m/Y",strtotime($image[$i]->earth_date)) . '</earth_date>';
     $rssfeed .= '<rover_name>' . $image[$i]->rover->name. '</rover_name>';
     $rssfeed .= '<pubDate>' . date("D, d M Y H:i:s O", strtotime($image[$i]->earth_date)) . '</pubDate>';

e0592704e9486159d4baef2e4bbe7d0a5b5cf39a
Joel Garcia Jr <joel.garciajr84@gmail.com>
2015-11-10 00:19:58 -0200

    remove debug

--- rover.php
+++ rover.php
@@ -1,9 +1,5 @@
 <?php
 //Autloload
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 function __autoload($class_name) {
     require_once "Class_".$class_name . '.php';
 }
